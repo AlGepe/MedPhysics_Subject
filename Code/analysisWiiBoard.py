469431cca21ba14dfab4c84d291a1cb3190e1c5e
AlGepe <alvarodiezgp@gmail.com>
2018-03-07 12:02:27 +0100

    End of lecture

--- Code/analysisWiiBoard.py
+++ Code/analysisWiiBoard.py
@@ -44,12 +44,18 @@ def file2dataWtags(fileName, tags):
 
     wbr = read.ReadManager(fileName+'.obci.xml', fileName+'.obci.raw',
                            fileName + '.obci.tag')
-    smart_tags = wii_cut_fragments(wbr, start_tag_name='ss_start',
-                                   end_tags_names=['ss_stop'])
-    TL = smart_tags[0].get_samples()[0, :]
-    TR = smart_tags[0].get_samples()[1, :]
-    BR = smart_tags[0].get_samples()[2, :]
-    BL = smart_tags[0].get_samples()[3, :]
+    startTag = tags[0]
+    endTag = tags[1]
+    print(startTag)
+    print(endTag)
+    cropped_by_tag = wii_cut_fragments(wbr, start_tag_name=startTag,
+                                       end_tags_names=[endTag])
+    print(len(cropped_by_tag))
+    TL = cropped_by_tag[0].get_samples()[0, :]
+    TR = cropped_by_tag[0].get_samples()[1, :]
+    BR = cropped_by_tag[0].get_samples()[2, :]
+    BL = cropped_by_tag[0].get_samples()[3, :]
+    TIME = cropped_by_tag[0].get_samples()[4, :]
 
     x = ((TR+BR) - (TL+BL)) / (TR+TL+BR+BL)
     y = ((TR+TL) - (BR+BL)) / (TR+TL+BR+BL)
@@ -57,31 +63,41 @@ def file2dataWtags(fileName, tags):
     x = x * 22.5  # Convert to cm
     y = y * 13  # Convert to cm
 
-    data = np.array(x, y)
+    data = np.array([TIME, x, y])
 
     return data
 
 
-allTheFiles = [os.path.abspath('../') + '/Data/']*7
+allTheFiles = [os.path.abspath('../')+'/Data/']*3
 index = len(allTheFiles[0])
 
-allTheFiles[0] += "still_eyes_closed_eyes_open"
+allTheFiles[0] += "sway_left"
+allTheFiles[1] += "sway_right"
+allTheFiles[2] += "sway_forward"
+"""
+allTheFiles[1] += "still_eyes_closed_eyes_open"
 allTheFiles[1] += "sway_back"
 allTheFiles[2] += "sway_forward"
 allTheFiles[3] += "sway_left"
 allTheFiles[4] += "sway_right"
 allTheFiles[5] += "sway_stay_with_feedback"
 allTheFiles[6] += "sway_with_feedback"
-i = 1
-allTheFiles
+tags = np.ndarray(
+"""
 for name in allTheFiles:
     print(name)
-    currentData = file2data(name)
+    tags = np.array([["start_fast", "stop_fast"],
+                     ["start", "stop"]],
+                    dtype=object)
+    '''
+    tags = np.array([["ss_start", "ss_stop"],
+                     ["ss_eyes_closed_start", "ss_eyes_closed_stop"]],
+                    dtype=object)
+    '''
+    currentData = file2dataWtags(name, tags[0])
     # create a subplot
+    currentData[0] -= min(currentData[0])
     plt.plot(currentData[1], currentData[2], 'o-')
-    plt.title(name[index-1:])
-    plt.axis([-22.5, 22.5, -13, 13])
+    plt.title(name[index:])
+    # plt.axis([-22.5, 22.5, -13, 13])
     plt.show()
-    i += 1
-
-
