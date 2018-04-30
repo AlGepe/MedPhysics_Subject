gotta fix CI point calculation

Kinect-3D/Code/analysis3D.py
@@ -4,6 +4,8 @@ import numpy as np
 # import matplotlib
 # matplotlib.use('pdf')
 import matplotlib.pyplot as plt
+folder = os.path.abspath('../') + '/Data/Results/'
+
 
 myDPI = 100
 
@@ -86,14 +88,14 @@ plt.legend(loc='best')
 plt.xlabel('Time / s')
 name = "All_data_Y-axis"
 plt.title(name)
-plt.savefig(name+'.png', dpi=myDPI)
+plt.savefig(folder + name+'.png', dpi=myDPI)
 # plt.show()
 # PLOT ONE JUMP
 plt.axis([13, 18, -800, 1050])
 plt.legend(loc='upper left')
 name += '_One_Jump'
 plt.title(name)
-plt.savefig(name+'.png', dpi=myDPI)
+plt.savefig(folder + name+'.png', dpi=myDPI)
 #plt.show()
 plt.close()
 
@@ -102,7 +104,7 @@ plt.plot(time, head[1], label='Head')
 plt.xlabel('Time / s')
 name = "Head_Y-Axis"
 plt.title(name)
-plt.savefig(name+'.png')
+plt.savefig(folder + name+'.png', dpi=myDPI)
 
 # PLOT HEAD w/ KNEE AND FOOT
 plt.plot(time, rKnee[1], label='Right Knee')
@@ -110,7 +112,7 @@ plt.plot(time, rFoot[1], label='Right Foot')
 plt.legend(loc='upper left')
 name = "Y-Axis_compare_Head_Knee_Foot_one_side"
 plt.title(name)
-plt.savefig(name+'.png', dpi=myDPI)
+plt.savefig(folder + name+'.png', dpi=myDPI)
 # plt.show()
 plt.close()
 
@@ -125,7 +127,7 @@ plt.plot(time, fnc.commonBegining(rKnee)[1], label='Right Knee')
 plt.plot(time, fnc.commonBegining(rHip)[1], label='Right Hip')
 plt.legend(loc='best')
 plt.title(name)
-plt.savefig(name+'.png', dpi=myDPI)
+plt.savefig(folder + name+'.png', dpi=myDPI)
 # plt.show()
 plt.close()
 
@@ -141,18 +143,25 @@ rFoot_cut = fnc.splitJumps(rFoot, separator)
 lFoot_cut = fnc.splitJumps(lFoot, separator)
 time_cut = fnc.splitJumps(time, separator)
 for i in range(len(separator)):
+    ciPoint = fnc.getCIindex(rFoot_cut[i], lFoot_cut[i])
     pfPoint = fnc.getPFindex(rKnee_cut[i], lKnee_cut[i], rFoot_cut[i],
                              lFoot_cut[i])
-    print('in main file pfReturned is: {0}'.format(pfPoint))
-    plt.plot(time_cut[i], rKnee_cut[i][1], 'b')
+    print('in main file ciReturned is: {0}'.format(ciPoint))
+    plt.plot(time_cut[i], rKnee_cut[i][1], 'm')
     plt.plot(time_cut[i], lKnee_cut[i][1], 'r')
     plt.plot(time_cut[i], rFoot_cut[i][1], 'g')
     plt.plot(time_cut[i], lFoot_cut[i][1], 'k')
+    plt.plot(np.full(100, time_cut[i][ciPoint]), np.linspace(-1000, 1000, 100),
+             'y-', label='CI')
     plt.plot(np.full(100, time_cut[i][pfPoint]), np.linspace(-1000, 1000, 100),
              'g-', label='PfKNEE')
+plt.legend(loc='best')
 plt.show()
 
 
+########
+# Calculate CI for each jump and valgus analysis
+########
 
 '''
 

Kinect-3D/Code/myUtils.py
@@ -71,18 +71,13 @@ def meaningFullOnly(oneJoint, t0=3.5):
 # PF [peak flexion] is defined here as the point of minimum distance between
 # the foot and the knee
 def getPFindex(rKnee, lKnee, rFoot, lFoot):
-    print("in PF Function")
     # peak flexion/ maximum squatting
     # Check only on Y axis (vertical)
     rSide = abs(abs(rKnee[1]) - abs(rFoot[1]))
     lSide = abs(abs(lKnee[1]) - abs(lFoot[1]))
     rPf = np.argmin(rSide)  # , axis=1)
     lPf = np.argmin(lSide)  # , axis=1)
-    print("Len of rSide {0}".format(len(rSide)))
-    print("Len of lSide {0}".format(len(lSide)))
-    print(max(rPf, lPf))
     if (rPf == lPf):
-        print(rPf)
         return rPf
     else:
         radius = 5
@@ -92,10 +87,8 @@ def getPFindex(rKnee, lKnee, rFoot, lFoot):
                           lSide[lPf-radius:lPf+radius])
 
         if(min(distAround_lPf) < min(distAround_rPf)):  # lPf is the best PF
-            print(np.argmax(distAround_lPf) + (lPf - radius))
             return np.argmin(distAround_lPf) + (lPf - radius)
         else:  # rPf is the best PF or they are both equal
-            print(np.argmax(distAround_rPf) + (rPf - radius))
             return np.argmin(distAround_rPf) + (rPf - radius)
 
 
@@ -134,9 +127,32 @@ def splitJumps(joint, starts, lenJump=75):
 
 
 # Returns the position in the array of the CI point
-def getCIindex(jointNp):
-    # Contact point
-    return np.argmin(jointNp, axis=1)
+def getCIindex(rFoot, lFoot):
+    # Code find the lowest knee point (~PF) and thensearches for the lowest
+    # foot position
+    high = [np.argmax(rFoot[1]), np.argmax(lFoot[1])]
+    rSide = np.diff(rFoot[1][high[0]:high[0] + 30])
+    lSide = np.diff(lFoot[1][high[1]:high[1] + 30])
+    rPf = np.argmin(rSide)  # , axis=1)
+    lPf = np.argmin(lSide)  # , axis=1)
+    print("Len of rSide {0}".format(len(rSide)))
+    print("Len of lSide {0}".format(len(lSide)))
+    print(max(rPf, lPf))
+    if (rPf == lPf):
+        return rPf + high[0]
+    else:
+        radius = 5
+        distAround_rPf = (rSide[rPf-radius:rPf+radius] +
+                          lSide[rPf-radius:rPf+radius])
+        distAround_lPf = (rSide[lPf-radius:lPf+radius] +
+                          lSide[lPf-radius:lPf+radius])
+
+        if(min(distAround_lPf) < min(distAround_rPf)):  # lPf is the best PF
+            print(np.argmin(distAround_lPf) + (lPf - radius) + high[1])
+            return np.argmin(distAround_lPf) + (lPf - radius + high[1])
+        else:  # rPf is the best PF or they are both equal
+            print(np.argmin(distAround_rPf) + (rPf - radius) + high[0])
+            return np.argmin(distAround_rPf) + (rPf - radius + high[0])
 
 
 def getRightKnee(joints):